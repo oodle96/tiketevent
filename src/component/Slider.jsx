2ca0ee5c4ef11650c0ae3cc2a1f575dcde784bfe
Macbook 2017 <macbook2017@sandi.local>
2022-10-10 02:55:55 +0700

    commit

---
+++ src/component/Slider.jsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../styles/Slider.css";
+
+import BannerSlider from '../assets/exbanner-slider.png'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        loopedSlides={1}
+        centeredSlides={true}
+        autoplay={{
+            delay: 4500,
+        }}
+
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[ Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <div className="d-flex align-items-center justify-content-center">
+                <img className='img-fluid mb-5 pt-5' src={BannerSlider} alt='bannerslider'></img>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div className="d-flex align-items-center justify-content-center">
+                <img className='img-fluid mb-5 pt-5' src={BannerSlider} alt='bannerslider'></img>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="d-flex align-items-center justify-content-center">
+                <img className='img-fluid mb-5 pt-5' src={BannerSlider} alt='bannerslider'></img>
+            </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
